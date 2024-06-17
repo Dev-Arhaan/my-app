b233555e0eb102edc3a8e867e42ec1ce4651e113
Dev-Arhaan <dev.sayarhaan@gmail.com>
2024-06-17 10:57:53 +0530

    updated config settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['avatars.githubusercontent.com']
-    }
-};
-
-export default nextConfig;
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
